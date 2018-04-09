e1018422e5957ac8d7f783fc97a529a616de54f0
nahtonaj <jonathan-gao@hotmail.com>
2018-04-09 00:58:24 -0400

    Fixed muxOSEL CNVZ logic

--- lab5a_dist/muxOSEL.v
+++ lab5a_dist/muxOSEL.v
@@ -1,13 +1,13 @@
-module muxOSEL(ADD_Y, ADD_C, ADD_N, ADD_V, ADD_Z,
-					SHIFT_Y, SHIFT_C, SHIFT_N, SHIFT_V, SHIFT_Z,
-					LOGIC_Y, LOGIC_C, LOGIC_N, LOGIC_V, LOGIC_Z,
+module muxOSEL(ADD_Y, ADD_C, ADD_V,
+					SHIFT_Y, SHIFT_C, SHIFT_V,
+					LOGIC_Y, LOGIC_C, LOGIC_V,
 					OSEL, Y, C, N, V, Z);
 
 	input [1:0] OSEL;
 	input [7:0] ADD_Y, SHIFT_Y, LOGIC_Y;
-	input ADD_C, ADD_N, ADD_V, ADD_Z;
-	input SHIFT_C, SHIFT_N, SHIFT_V, SHIFT_Z;
-	input LOGIC_C, LOGIC_N, LOGIC_V, LOGIC_Z;
+	input ADD_C, ADD_V;
+	input SHIFT_C, SHIFT_V;
+	input LOGIC_C, LOGIC_V;
 	
 	output reg C, N, V, Z;
 	output [7:0] reg Y;
@@ -19,24 +19,20 @@ module muxOSEL(ADD_Y, ADD_C, ADD_N, ADD_V, ADD_Z,
 	always @(*) begin
 		if (OSEL == SHIFT) begin
 			C <= SHIFT_C;
-			N <= SHIFT_N;
 			V <= SHIFT_V;
-			Z <= SHIFT_Z;
 			Y <= SHIFT_Y;
 		end
 		else if (OSEL == LOGIC) begin
 			C <= LOGIC_C;
-			N <= LOGIC_N;
 			V <= LOGIC_V;
-			Z <= LOGIC_Z;
 			Y <= LOGIC_Y;
 		end
 		else begin // default is adder
 			C <= ADD_C;
-			N <= ADD_N;
 			V <= ADD_V;
-			Z <= ADD_Z;
 			Y <= ADD_Y;
 		end
+		N <= Y[7];
+		Z <= ~(Y[7] | Y[6] | Y[5] | Y[4] | Y[3] | Y[2] | Y[1] | Y[0]);
 	end
 endmodule
