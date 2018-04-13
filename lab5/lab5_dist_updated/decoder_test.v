2d6257d6ac373ced4dc5dc33ddc6839e7578fc95
nahtonaj <jonathan-gao@hotmail.com>
2018-04-12 20:43:43 -0400

    Finished first 9 test cases

--- lab5/lab5_dist_updated/decoder_test.v
+++ lab5/lab5_dist_updated/decoder_test.v
@@ -283,6 +283,184 @@ module decoder_test();
     
     totalTests = totalTests + 1;
 	 
+	 //-------------------------------------------------------------------
+    // Test #6 ORI
+    //-------------------------------------------------------------------
+    INST =    16'b0111000001000010;
+    DR_REF =  3'b001;
+    SA_REF =  3'b000;
+    SB_REF =  3'b0; //Don't care
+    IMM_REF = 6'b000010;
+    MB_REF =  1'b1;
+    FS_REF =  3'b110; //OR
+    MD_REF =  1'b0;
+    LD_REF =  1'b1;
+    MW_REF =  1'b0;
+    
+    #100;
+  
+    $display("MSIM> ");
+    $display("MSIM> #%2d", totalTests);
+    
+    // print a different message depending on whether this is a
+    // register-to-register (r2r) or immediate (imm) instruction
+    if (INST[15] == 1)
+      $display("MSIM>  INST = [%4b][%3b][%3b][%3b][%3b] (r2r)", INST[15:12], INST[11:9], INST[8:6], INST[5:3], INST[2:0]);
+    else
+      $display("MSIM>  INST = [%4b][%3b][%3b][%6b] (imm)", INST[15:12], INST[11:9], INST[8:6], INST[5:0]);
+    
+    $display("MSIM>   DR  = %3b\t\t\t[%s]", DR, DR==DR_REF ? " OK " : "FAIL");
+    $display("MSIM>   SA  = %3b\t\t\t[%s]", SA, SA==SA_REF ? " OK " : "FAIL");
+    $display("MSIM>   SB  = %3b\t\t\t[%s]", SB, SB==SB_REF ? " OK " : "FAIL");
+    $display("MSIM>   IMM = %6b\t\t[%s]",   IMM, IMM==IMM_REF ? " OK " : "FAIL");
+    $display("MSIM>   MB  = %1b\t\t\t[%s]", MB, MB==MB_REF ? " OK " : "FAIL");
+    $display("MSIM>   FS  = %3b\t\t\t[%s]", FS, FS==FS_REF ? " OK " : "FAIL");
+    $display("MSIM>   MD  = %1b\t\t\t[%s]", MD, MD==MD_REF ? " OK " : "FAIL");
+    $display("MSIM>   LD  = %1b\t\t\t[%s]", LD, LD==LD_REF ? " OK " : "FAIL");
+    $display("MSIM>   MW  = %1b\t\t\t[%s]", MW, MW==MW_REF ? " OK " : "FAIL");
+    
+    if((DR!=DR_REF) || (SA!=SA_REF) || (SB!=SB_REF) || (IMM!=IMM_REF) ||
+       (MB!=MB_REF) || (FS!=FS_REF) || (MD!=MD_REF) || (LD!=LD_REF) || (MW!=MW_REF))
+    begin
+      numTestsFailed = numTestsFailed + 1;
+    end
+    
+    totalTests = totalTests + 1;
+	 
+	 //-------------------------------------------------------------------
+    // Test #7 ADD
+    //-------------------------------------------------------------------
+    INST =    16'b1111000001010000;
+    DR_REF =  3'b010;
+    SA_REF =  3'b000;
+    SB_REF =  3'b001; 
+    IMM_REF = 6'b010000;
+    MB_REF =  1'b0;
+    FS_REF =  3'b000; //ADD
+    MD_REF =  1'b0;
+    LD_REF =  1'b1;
+    MW_REF =  1'b0;
+    
+    #100;
+  
+    $display("MSIM> ");
+    $display("MSIM> #%2d", totalTests);
+    
+    // print a different message depending on whether this is a
+    // register-to-register (r2r) or immediate (imm) instruction
+    if (INST[15] == 1)
+      $display("MSIM>  INST = [%4b][%3b][%3b][%3b][%3b] (r2r)", INST[15:12], INST[11:9], INST[8:6], INST[5:3], INST[2:0]);
+    else
+      $display("MSIM>  INST = [%4b][%3b][%3b][%6b] (imm)", INST[15:12], INST[11:9], INST[8:6], INST[5:0]);
+    
+    $display("MSIM>   DR  = %3b\t\t\t[%s]", DR, DR==DR_REF ? " OK " : "FAIL");
+    $display("MSIM>   SA  = %3b\t\t\t[%s]", SA, SA==SA_REF ? " OK " : "FAIL");
+    $display("MSIM>   SB  = %3b\t\t\t[%s]", SB, SB==SB_REF ? " OK " : "FAIL");
+    $display("MSIM>   IMM = %6b\t\t[%s]",   IMM, IMM==IMM_REF ? " OK " : "FAIL");
+    $display("MSIM>   MB  = %1b\t\t\t[%s]", MB, MB==MB_REF ? " OK " : "FAIL");
+    $display("MSIM>   FS  = %3b\t\t\t[%s]", FS, FS==FS_REF ? " OK " : "FAIL");
+    $display("MSIM>   MD  = %1b\t\t\t[%s]", MD, MD==MD_REF ? " OK " : "FAIL");
+    $display("MSIM>   LD  = %1b\t\t\t[%s]", LD, LD==LD_REF ? " OK " : "FAIL");
+    $display("MSIM>   MW  = %1b\t\t\t[%s]", MW, MW==MW_REF ? " OK " : "FAIL");
+    
+    if((DR!=DR_REF) || (SA!=SA_REF) || (SB!=SB_REF) || (IMM!=IMM_REF) ||
+       (MB!=MB_REF) || (FS!=FS_REF) || (MD!=MD_REF) || (LD!=LD_REF) || (MW!=MW_REF))
+    begin
+      numTestsFailed = numTestsFailed + 1;
+    end
+    
+    totalTests = totalTests + 1;
+	 
+	 //-------------------------------------------------------------------
+    // Test #8 SUB
+    //-------------------------------------------------------------------
+    INST =    16'b1111000001010001;
+    DR_REF =  3'b010;
+    SA_REF =  3'b000;
+    SB_REF =  3'b001; 
+    IMM_REF = 6'b010001;
+    MB_REF =  1'b0;
+    FS_REF =  3'b001; //SUB
+    MD_REF =  1'b0;
+    LD_REF =  1'b1;
+    MW_REF =  1'b0;
+    
+    #100;
+  
+    $display("MSIM> ");
+    $display("MSIM> #%2d", totalTests);
+    
+    // print a different message depending on whether this is a
+    // register-to-register (r2r) or immediate (imm) instruction
+    if (INST[15] == 1)
+      $display("MSIM>  INST = [%4b][%3b][%3b][%3b][%3b] (r2r)", INST[15:12], INST[11:9], INST[8:6], INST[5:3], INST[2:0]);
+    else
+      $display("MSIM>  INST = [%4b][%3b][%3b][%6b] (imm)", INST[15:12], INST[11:9], INST[8:6], INST[5:0]);
+    
+    $display("MSIM>   DR  = %3b\t\t\t[%s]", DR, DR==DR_REF ? " OK " : "FAIL");
+    $display("MSIM>   SA  = %3b\t\t\t[%s]", SA, SA==SA_REF ? " OK " : "FAIL");
+    $display("MSIM>   SB  = %3b\t\t\t[%s]", SB, SB==SB_REF ? " OK " : "FAIL");
+    $display("MSIM>   IMM = %6b\t\t[%s]",   IMM, IMM==IMM_REF ? " OK " : "FAIL");
+    $display("MSIM>   MB  = %1b\t\t\t[%s]", MB, MB==MB_REF ? " OK " : "FAIL");
+    $display("MSIM>   FS  = %3b\t\t\t[%s]", FS, FS==FS_REF ? " OK " : "FAIL");
+    $display("MSIM>   MD  = %1b\t\t\t[%s]", MD, MD==MD_REF ? " OK " : "FAIL");
+    $display("MSIM>   LD  = %1b\t\t\t[%s]", LD, LD==LD_REF ? " OK " : "FAIL");
+    $display("MSIM>   MW  = %1b\t\t\t[%s]", MW, MW==MW_REF ? " OK " : "FAIL");
+    
+    if((DR!=DR_REF) || (SA!=SA_REF) || (SB!=SB_REF) || (IMM!=IMM_REF) ||
+       (MB!=MB_REF) || (FS!=FS_REF) || (MD!=MD_REF) || (LD!=LD_REF) || (MW!=MW_REF))
+    begin
+      numTestsFailed = numTestsFailed + 1;
+    end
+    
+    totalTests = totalTests + 1;
+	 
+	 //-------------------------------------------------------------------
+    // Test #9 SRA
+    //-------------------------------------------------------------------
+    INST =    16'b1111000001010010;
+    DR_REF =  3'b010;
+    SA_REF =  3'b000;
+    SB_REF =  3'b001; //Don't care
+    IMM_REF = 6'b010010;
+    MB_REF =  1'b0;
+    FS_REF =  3'b010; //SRA
+    MD_REF =  1'b0;
+    LD_REF =  1'b1;
+    MW_REF =  1'b0;
+    
+    #100;
+  
+    $display("MSIM> ");
+    $display("MSIM> #%2d", totalTests);
+    
+    // print a different message depending on whether this is a
+    // register-to-register (r2r) or immediate (imm) instruction
+    if (INST[15] == 1)
+      $display("MSIM>  INST = [%4b][%3b][%3b][%3b][%3b] (r2r)", INST[15:12], INST[11:9], INST[8:6], INST[5:3], INST[2:0]);
+    else
+      $display("MSIM>  INST = [%4b][%3b][%3b][%6b] (imm)", INST[15:12], INST[11:9], INST[8:6], INST[5:0]);
+    
+    $display("MSIM>   DR  = %3b\t\t\t[%s]", DR, DR==DR_REF ? " OK " : "FAIL");
+    $display("MSIM>   SA  = %3b\t\t\t[%s]", SA, SA==SA_REF ? " OK " : "FAIL");
+    $display("MSIM>   SB  = %3b\t\t\t[%s]", SB, SB==SB_REF ? " OK " : "FAIL");
+    $display("MSIM>   IMM = %6b\t\t[%s]",   IMM, IMM==IMM_REF ? " OK " : "FAIL");
+    $display("MSIM>   MB  = %1b\t\t\t[%s]", MB, MB==MB_REF ? " OK " : "FAIL");
+    $display("MSIM>   FS  = %3b\t\t\t[%s]", FS, FS==FS_REF ? " OK " : "FAIL");
+    $display("MSIM>   MD  = %1b\t\t\t[%s]", MD, MD==MD_REF ? " OK " : "FAIL");
+    $display("MSIM>   LD  = %1b\t\t\t[%s]", LD, LD==LD_REF ? " OK " : "FAIL");
+    $display("MSIM>   MW  = %1b\t\t\t[%s]", MW, MW==MW_REF ? " OK " : "FAIL");
+    
+    if((DR!=DR_REF) || (SA!=SA_REF) || (SB!=SB_REF) || (IMM!=IMM_REF) ||
+       (MB!=MB_REF) || (FS!=FS_REF) || (MD!=MD_REF) || (LD!=LD_REF) || (MW!=MW_REF))
+    begin
+      numTestsFailed = numTestsFailed + 1;
+    end
+    
+    totalTests = totalTests + 1;
+	 
+	 
+	 
 	 
     //-------------------------------------------------------------------
     // END OF TESTS
